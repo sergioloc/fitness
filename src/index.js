de163231f67183a0c949a5f704de702711bdd0f5
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-06 17:35:03 +0200

    New exercises page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Card from './components/Card.js'
-import Welcome from './components/Welcome.js'
 import 'bootstrap/dist/css/bootstrap.css'
+import Exercises from './pages/Exercises'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(
-<div>
-    <Welcome
-        username="friend"
-    />
-    <Card 
-        title="Technique Guides"
-        description="Learn amazing street workout and calisthenics"
-        img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-        leftColor="#A74CF2"
-        rightColor="#617BFB"
-    />
-</div>, container)
+ReactDOM.render(<Exercises/>, container)
