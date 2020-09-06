f93196549129aca072139b889afbb8a0306d5df2
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-06 16:32:22 +0200

    Hello world

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element = 'Hello world'
+const container = document.getElementById('root')
+
+ReactDOM.render(element, container)
