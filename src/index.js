44f24bb610be2f7955f3b613ad324b08d71a9307
Sergio López Ceballos <sergioloc2@gmail.com>
2020-09-06 16:35:33 +0200

    New card component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Card from './components/Card.js'
 
-const element = 'Hello world'
 const container = document.getElementById('root')
 
-ReactDOM.render(element, container)
+ReactDOM.render(<Card />, container)
